bda07e30f49bde3efcf4bec7779b83ab7eabb6f7
JOHAN DAVID SOLANO MOREIRA <jdsolano@edu.upolitecnica.cr>
2024-07-25 20:30:59 -0600

    Cambios en controlador de archivos

---
+++ ClaseDeControlador/ControladorDeArchivosClientes.cs
@@ -0,0 +1,31 @@
+﻿using ClaseDeControlador.Interfaces;
+using ClaseDeModelo.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseDeControlador
+{
+    internal class ControladorDeArchivosClientes : IManejadorDeArchivoClientes
+    {
+        public string CargarArchivo(string path)
+        {
+
+            throw new NotImplementedException();
+        }
+
+        public List<IClientes> CargarClientes(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool GuardarArchivo(string path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
+
---
+++ ClaseDeControlador/Interfaces/IManejadorDeArchivoClientes.cs
@@ -0,0 +1,35 @@
+﻿using ClaseDeModelo.Interfaces;
+
+
+
+namespace ClaseDeControlador.Interfaces
+{
+    /// <summary>
+    /// Interface for file handle
+    /// </summary>
+    public interface IManejadorDeArchivoClientes
+    {
+        /// <summary>
+        /// cagar archivo de clientes
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        string CargarArchivo(string path);
+        /// <summary>
+        ///  cargar clientes
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+
+        List<IClientes> CargarClientes(string path);
+
+        /// <summary>
+        /// guardar archivo
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+
+
+        bool GuardarArchivo(string path);
+    }
+}
